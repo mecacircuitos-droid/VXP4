c23044c030bdf5c3a6e67c182364dbebb6cb918c
mecacircuitos-droid <mecacircuitos@gmail.com>
2026-02-08 20:22:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
      #st.markdown(
         # "<div class='vxp-shell-statusbar'><div>READY</div><div></div></div>",
          # unsafe_allow_html=True,
-    )
+     #)
 
 if __name__ == "__main__":
     main()
