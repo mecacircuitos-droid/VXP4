339017488f5aad909b78fea286d18972e4ae79cd
mecacircuitos-droid <mecacircuitos@gmail.com>
2026-02-08 20:20:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -38,6 +38,10 @@ def main():
     with right:
         render_desktop()
 
+    # Barra de estado (simple)
+    st.markdown(
+        "<div class='vxp-shell-statusbar'><div>READY</div><div></div></div>",
+        unsafe_allow_html=True,
     )
 
 if __name__ == "__main__":
